28c29e80e46b707f859d36d2523f0c4a8986b14b
environerliu-collab <environerliu@gmail.com>
2025-08-25 12:37:09 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(SCD40.get_relative_humidity(), 100)
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     let co2 = 0
     if (co2 >= 1000) {
@@ -9,9 +12,13 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
             . . . . .
             . . # . .
             `)
-        basic.pause(250)
+        basic.pause(100)
     } else if (co2 < 1000) {
         basic.setLedColor(0x00ff00)
-        basic.showIcon(IconNames.Happy, 250)
+        basic.showIcon(IconNames.Happy, 100)
     }
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(SCD40.get_temperature(SCD40.SCD40_T_UNIT.C), 100)
+})
+basic.showString("Ich bin CO2 ", 100)
